2b250ec54d6ecc9c62f2b991747438a440d571aa
Tsjippy <ewaldha@hotmail.com>
2026-05-27 12:29:30 +0200

    fixed: ?? bug

--- php/main.php
+++ php/main.php
@@ -25,7 +25,7 @@ function addAttachment( $postId) {
 add_filter('display_post_states', __NAMESPACE__.'\postStates', 10, 2);
 function postStates( $states, $post ) {
 
-	if ( $post->ID == SETTINGS['mediagallery-pages'] ?? '') {
+	if ( $post->ID == (SETTINGS['mediagallery-pages'] ?? '')) {
 		$states[] = __('Media gallery page');
 	}
 
