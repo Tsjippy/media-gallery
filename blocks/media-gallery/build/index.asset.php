f15ed1f6f75a1b8db060222ddfa01b0f1648a33a
Tsjippy <ewaldha@hotmail.com>
2026-07-10 15:54:33 +0200

    changed: block name

--- blocks/media-gallery/build/index.asset.php
+++ blocks/media-gallery/build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '1581046017fd6889e57d');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '14116a454d35f6689b7c');
